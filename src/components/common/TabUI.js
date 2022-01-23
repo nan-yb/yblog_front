54298431e3729b3cf6f4ea5bfb6cd56afc2eda1a
nan-yb <dydqls5757@gmail.com>
2022-01-23 11:58:52 +0900

    jenkins CI/CD 테스트

--- src/components/common/TabUI.js
+++ src/components/common/TabUI.js
@@ -12,7 +12,7 @@ const TabUI = ({ prop }) => {
   const boardList = prop;
   const articleList = prop[0].content;
 
-  if (!articleList) return <div>비어있음 z</div>;
+  if (!articleList) return <div>비어있음</div>;
 
   return (
     <div className="max-w-md px-2 py-16 sm:px-0 w-72">
